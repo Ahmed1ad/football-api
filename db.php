0001963d1cc68bcf1fffd2228e3503697fb37fed
Ahmed1ad <96233908+Ahmed1ad@users.noreply.github.com>
2026-01-26 00:15:31 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,15 @@
 <?php
 
-$dsn = "pgsql:host=ep-xxx.neon.tech;dbname=YOUR_DB;sslmode=require";
-$user = "YOUR_USER";
-$pass = "YOUR_PASS";
+$dsn = "pgsql:host=ep-bold-rice-a4pm6qq-pooler.c-3.us-east-1.aws.neon.tech;dbname=neondb;sslmode=require";
 
-$pdo = new PDO($dsn, $user, $pass);
+$user = "neondb_owner";
+$pass = "ngB_1HprOf0a5M";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("DB Connection Failed");
+}
