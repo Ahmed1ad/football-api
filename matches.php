90a65c3c9c1dfa6dae89cf0d0f0ab51f1f57cdb7
Ahmed1ad <96233908+Ahmed1ad@users.noreply.github.com>
2026-01-27 05:05:48 +0200

    Delete matches.php

--- matches.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json; charset=UTF-8");
-
-include "db.php";
-
-$stmt = $pdo->query("SELECT * FROM matches ORDER BY match_time");
-
-echo json_encode($stmt->fetchAll());
